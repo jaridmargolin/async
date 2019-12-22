7a8256784cb9296f820de4b0fc9f948e9b23af5a
Jarid Margolin <jaridmargolin@gmail.com>
2019-12-21 23:18:32 -0800

    Update build process to utilize core-js

--- rollup.config.js
+++ rollup.config.js
@@ -4,19 +4,29 @@
  * dependencies
  * -------------------------------------------------------------------------- */
 
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import babel from 'rollup-plugin-babel'
-import resolve from 'rollup-plugin-node-resolve'
 
 /* -----------------------------------------------------------------------------
  * rollup config
  * -------------------------------------------------------------------------- */
 
+const externals = [
+  'core-js/modules/es.date.now',
+  'core-js/modules/es.object.to-string',
+  'core-js/modules/es.object.assign',
+  'core-js/modules/es.date.to-string',
+  'core-js/modules/es.promise',
+  'core-js/modules/es.promise.finally'
+]
+
 const sharedPlugins = [
   resolve({
-    extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx', '.json'],
-    module: true
+    extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx', '.json']
   }),
+  commonjs(),
   babel({
     extensions: ['.ts', '.tsx', '.mjs', '.js', '.jsx'],
     exclude: 'node_modules/**'
@@ -26,16 +36,7 @@ const sharedPlugins = [
 export default [
   {
     input: 'src/index.ts',
-    plugins: [...sharedPlugins],
-    output: {
-      file: 'dist/async.js',
-      format: 'umd',
-      name: 'Async',
-      exports: 'named'
-    }
-  },
-  {
-    input: 'src/index.ts',
+    external: externals,
     plugins: [...sharedPlugins],
     output: {
       file: 'dist/common/async.js',
@@ -45,10 +46,21 @@ export default [
   },
   {
     input: 'src/index.ts',
+    external: externals,
     plugins: [...sharedPlugins],
     output: {
       file: 'dist/es/async.js',
       format: 'es'
+    }
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [...sharedPlugins],
+    output: {
+      file: 'dist/async.js',
+      format: 'umd',
+      name: 'Async',
+      exports: 'named'
     }
   },
   {
